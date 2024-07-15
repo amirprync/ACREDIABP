8f623169c66382d4f4e76d5b1df945add363309b
amirprync <60889238+amirprync@users.noreply.github.com>
2024-07-15 17:10:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def modify_record(record):
     # Aplicar el espacio y agregar la comilla entre "1" y "8000"
     record = re.sub(r'("1")("8000")', r'\1 "\2', record)
     # Eliminar el "0" "0" después de "8000"
-    record = re.sub(r'("8000")"0"0"', r'\1', record)
+    record = re.sub(r'("8000")"0""0"', r'\1', record)
     return record
 
 # Configuración de la página
